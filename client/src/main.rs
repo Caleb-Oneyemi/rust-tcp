fc17b60be8755cc119d435ce919d80fbf22d4e49
Caleb Oneyemi <caloneyemi@gmail.com>
2024-04-10 22:09:48 +0100

    added implementation for server and client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,43 @@
+use clap::Parser;
+use dotenv::dotenv;
+
+use std::{
+    env,
+    io::{Read, Write},
+    net::TcpStream,
+};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let addr = env::var("SERVER_ADDRESS").expect("SERVER_ADDRESS must be set");
+
+    let args = Args::parse();
+
+    if let Ok(mut stream) = TcpStream::connect(addr) {
+        println!("Connected to tcp server...");
+
+        let msg = format!("Hello {}", args.name);
+
+        stream.write(msg.as_bytes()).unwrap();
+
+        let mut buffer = [0; 1024];
+
+        stream.read(&mut buffer).unwrap();
+
+        let res = std::str::from_utf8(&buffer).expect("Found invalid UTF-8");
+
+        println!(
+            "Got response from server:{:?}",
+            res.trim_end_matches(char::from(0))
+        );
+    } else {
+        println!("Couldn't connect tcp server...");
+    }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,30 @@
+use dotenv::dotenv;
+use std::{
+    env,
+    io::{Read, Write},
+    net::TcpListener,
+};
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let addr = env::var("SERVER_ADDRESS").expect("SERVER_ADDRESS must be set");
+
+    let conn = TcpListener::bind(&addr).unwrap();
+
+    println!("tcp server running on {}", addr);
+
+    for stream in conn.incoming() {
+        let mut stream = stream.unwrap();
+
+        println!("connection established");
+
+        let mut buffer = [0; 1024];
+
+        stream.read(&mut buffer).unwrap();
+
+        stream.write(&mut buffer).unwrap();
+
+        stream.flush().unwrap();
+    }
 }
